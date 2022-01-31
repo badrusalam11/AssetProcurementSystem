81b239c0242e7e165cba772e8f5c361f0ca7c5ae
Timothy Yosefo Zebua <timothy.yosefo@gmail.com>
2022-01-31 13:20:57 +0700

    Form Login, Backend Login

--- API-project/Repository/data/AccountRepository.cs
+++ API-project/Repository/data/AccountRepository.cs
@@ -1,5 +1,6 @@
 ﻿using API_project.Context;
 using API_project.Models;
+using API_project.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,30 @@ namespace API_project.Repository.data
 {
     public class AccountRepository : GeneralRepository<MyContext, Account, string>
     {
+        private readonly MyContext myContext;
         public AccountRepository(MyContext myContext) : base(myContext)
         {
+            this.myContext = myContext;
+        }
+        public int Login(LoginVM loginVM)
+        {
+            var email = myContext.Accounts.FirstOrDefault(a => a.Email == loginVM.Email);
+            if (email != null)
+            {
+                var account = myContext.Accounts.Where(a => a.ID == email.ID).FirstOrDefault();
+                //if (BCrypt.Net.BCrypt.Verify(loginVM.Password, account.Password))
+                //{
+                    return 1;
+                //}
+                //return 6;
+            }
+            return 0;
+        }
+        public IEnumerable<Object> GetRoles(string mail)
+        {
+            var cek = myContext.Accounts.Where(a => a.Email == mail).FirstOrDefault();
+            var role = myContext.AccountRoles.Where(a => a.ID == cek.ID).Select(ar => ar.Role.Name).ToList();
+            return role;
         }
     }
 }
